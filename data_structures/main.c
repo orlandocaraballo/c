6b002361a21e07735b1c81837b3aff7146f1104a
Orlando Caraballo <orlandocaraballo@gmail.com>
2018-10-01 08:31:36 -0400

    Added new code for stack

--- data_structures/main.c
+++ data_structures/main.c
@@ -7,16 +7,25 @@ int main() {
   Stack *stack = (Stack *) malloc(sizeof(Stack));
 
   initialize(stack);
-  grow(stack);
   push(stack, 90);
   push(stack, -1);
   push(stack, -33);
   push(stack, 4);
-
+  push(stack, -13);
+  push(stack, 11);
+  push(stack, -9);
+  push(stack, 100);
+  
+  // printf("The number popped from the stack is: %d\n", pop(stack));
+  
+  pop(stack);
+  pop(stack);
 
   for(i = 0; i < stack->size; i++) {
     printf("The current element on index [%d] is: %d\n", i, stack->values[i]);
   }
+
+  printf("The number at the top of the stack is: %d\n", peek(stack));
 
   return 0;
 }
--- data_structures/stack.h
+++ data_structures/stack.h
@@ -1,45 +1,95 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct _stack {
   int *values;
   int count;
   int size;
   int multiplier;
-  // int (* peek)();
-  // void (* push)(int value);
-  // int (* pop)();
 } Stack;
 
 void initialize(Stack *stack) {
-  int array[] = { 9, 11, 13 };
-  stack->values = (int *) malloc(sizeof(int[3]));
-
-  stack->count = 3;
-  stack->size = 3;
+  stack->values = NULL;
+  stack->count = 0;
+  stack->size = 0;
   stack->multiplier = 2;
-  stack->values[0] = 9;
-  stack->values[1] = 11;
-  stack->values[2] = 13;
+}
+
+int calculate_new_size(Stack *stack) {
+  int new_size;
+
+  if(stack->size == 0) {
+    // if the stack is empty then set the size to 1
+    new_size = 1;
+  } else {
+    // otherwise set the new size based on the multiplier
+    new_size = stack->multiplier * stack->size;
+  }
+
+  return new_size;
 }
 
 void grow(Stack *stack) {
-  int larger_size = stack->multiplier * stack->size;
-  int *larger_array = (int *) malloc(sizeof(int[larger_size]));
+  // get the new size of the inner array
+  int larger_size = calculate_new_size(stack);
+
+  // create a new array
+  int *larger_array = (int *) calloc(larger_size, sizeof(int));
   int i;
 
+  // fill the new array with the data from the old array
   for(i = 0; i < stack->size; i++) {
     larger_array[i] = stack->values[i];
   }
 
+  // set the values pointer to the new array
   stack->values = larger_array;
+
+  // set the new size of the array to the larger size
   stack->size = larger_size;
 }
 
+// push items into the stack
 void push(Stack *stack, int value) {
+  // grow the stack if the inner array is full
+  //  for data
   if(stack->count == stack->size) {
     grow(stack);
   }
 
+  // add the value to the end of the inner array
   stack->values[stack->count] = value;
   stack->count++;
+}
+
+int peek(Stack *stack) {
+  // let the user know they can't peek with no data
+  if(stack->count == 0) {
+    printf("You cannot peek with no data stored in the stack\n");
+    exit(1);
+  }
+
+  return stack->values[stack->count - 1];
+}
+
+int pop(Stack *stack) {
+  // let the user know they can't pop with no data
+  if(stack->count == 0) {
+    printf("You cannot pop with no data stored in the stack\n");
+    exit(1);
+  }
+
+  int current_index = stack->count - 1;
+  
+  // store the popped value so we can return it from the function
+  int popped_value = stack->values[current_index];
+
+  // set the current last element to 0
+  stack->values[current_index] = 0;
+
+  // reduce the stack count by 1
+  stack->count--;
+
+  // return the last element of the inner array
+  return popped_value;
 }
