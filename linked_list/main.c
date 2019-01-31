55bbd3d754e88117e6294dd85e1ab20aa5772477
orlandocaraballo <orlando.caraballo@nycda.com>
2019-01-31 13:14:43 -0500

    Added a seed for the random values within linked list

--- linked_list/main.c
+++ linked_list/main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "linked_list.h"
 #include "../error.h"
 
 int main(int argument_count, char **argument_values) {
   LinkedList *list = initialize_linked_list();
 
+  // use the current time as the seed
+  //  for random function used below
+  srand(time(NULL));
+
   if(argument_count < 2) {
     print_error_and_exit("You must provide an argument.", 1);
   }
 
   for(int i = 0; i < atoi(argument_values[1]); ++i) {
+    // generate a random number to add to the linked list
     add_to_tail(list, rand() % 100 + 1);
   }
-
-  add_to_head(list, 25);
-  add_to_head(list, 9001);
 
   display(list);
 
